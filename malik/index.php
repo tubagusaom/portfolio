042918409047f4b0be4daf70250bdf8b0c7757f2
Tera Byte <tubagus.aom.swk@gmail.com>
2024-09-22 12:13:49 +0700

    mergemalik

--- malik/index.php
+++ malik/index.php
@@ -9,9 +9,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Malik</title>
 
-    <meta http-equiv='cache-control' content='no-cache'>
+    <!-- <meta http-equiv='cache-control' content='no-cache'>
     <meta http-equiv='expires' content='0'>
-    <meta http-equiv='pragma' content='no-cache'>
+    <meta http-equiv='pragma' content='no-cache'> -->
 
     <!-- <meta http-equiv="X-UA-Compatible" content="IE=edge" /> -->
 
